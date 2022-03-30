9bfc910cc4a9f8f054058880e1233adbc8ed304b
Anesu Kafesu <anesukafesu@gmail.com>
2022-03-30 10:24:51 +0200

    Fixed static file configuration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('/client/build'));
-app.get('/', (req, res) => res.sendFile(__dirname + 'client/build/index.html'));
+app.use(express.static('client/public'));
+app.get('/', (req, res) => res.sendFile(__dirname + 'client/public/index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
